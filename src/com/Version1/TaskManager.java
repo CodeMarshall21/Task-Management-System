183675664eb458ed9c5d6852949234b6c4361b5d
keshav <k21keshav05@gmail.com>
2025-09-09 16:40:29 +0530

    addTask() Logic

--- src/com/Version1/TaskManager.java
+++ src/com/Version1/TaskManager.java
@@ -1,7 +1,19 @@
 package com.Version1;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class TaskManager {
 
+    Scanner in = new Scanner(System.in);
+    private Map<Integer, Task> tasks = new HashMap<>();
+    int counter = 0;
+
     public void addTask() {
+        System.out.print("Enter Task Name: ");
+        String name = in.nextLine();
+        tasks.put(++counter, new Task(counter, name));
+        System.out.println("Task Added Successfully !");
     }
 }
